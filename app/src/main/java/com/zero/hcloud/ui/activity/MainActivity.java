55e2906a8cdb22642bdf6b85eb581a0643f1feaf
trieyouth <trieyouth@gmail.com>
2015-06-20 23:40:06 +0800

    hcloud init

---
+++ app/src/main/java/com/zero/hcloud/ui/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zero.hcloud.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zero.hcloud.R;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
